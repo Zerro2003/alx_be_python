c038a47e23225cb7a91e0eaa4676e5a12c26e418
Elie Mugisha <mugishaelie160@gmail.com>
2024-06-25 01:53:02 +0200

    just made additional changes to simple_interes.pu

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,5 @@
-principal= 1000
-rate= 0.05
-time= 3
-simpleint= principal* rate* time
+principal = 1000
+rate = 0.05
+time = 3
+simpleint = principal * rate * time
 print('the simple interest is:', simpleint)
